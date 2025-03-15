22c5687fbd1f210e8f787481c7a9134546eef6a2
giacuong171 <zin96bg@gmail.com>
2025-03-15 20:25:28 +0100

    Initial commit

---
+++ xxxx-start/src/main/java/com/xxxx/StartApplication.java
@@ -0,0 +1,16 @@
+package com.xxxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class StartApplication {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
